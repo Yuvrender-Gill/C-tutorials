ffcf7a5f6465c1fa61e4e4d1a53913fa65314890
Yuvrender Singh Gill <yuvrender.gill@mail.utoronto.ca>
2018-02-25 19:35:46 -0500

    Structs and String files

---
+++ structs.c
@@ -0,0 +1,25 @@
+#define MAX_POSITION_SIZE 16
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct player {
+    char *name;
+    char position[MAX_POSITION_SIZE];
+    int home_runs;
+    float avg;
+};
+
+
+}
+
+int main() {
+    // Declare a struct player named p1.
+
+    // Initialize it to represent the third baseman Josh Donaldson,
+    // whose batting average is 0.297. He has hit 41 home runs.
+
+    }
+
+}
+
